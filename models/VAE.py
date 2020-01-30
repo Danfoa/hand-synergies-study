f08131885c81f0a51a691176ee58b0d57cf05ba8
Daniel Ordonez <daniels.ordonez@gmail.com>
2020-01-30 19:03:36 +0100

    VAE  v0.0.2 - Tensorboard visualization of training

--- models/VAE.py
+++ models/VAE.py
@@ -2,6 +2,33 @@ import tensorflow as tf
 from tensorflow.python.keras import layers
 
 
+class KLLossLogger(tf.keras.callbacks.Callback):
+
+    def __init__(self, train_summary_writer, batch_size=1):
+        self.train_summary_writer = train_summary_writer
+        self.batch_size = batch_size
+        pass
+
+    def on_train_batch_end(self, batch, logs=None):
+        with self.train_summary_writer.as_default():
+            tf.summary.scalar('kl_loss_weight', self.model.kl_loss_weight, step=batch*self.batch_size)
+            # tf.summary.scalar('kl_loss', tf.keras.metrics.Mean(self.model.kl_loss).result(), step=batch*self.batch_size)
+    # def on_train_epoch_end(self, epocj, logs=None):
+        # print("Epoch: KL-Loss %.5f" % )
+
+class CyclicalAnnealingSchedule(tf.keras.callbacks.Callback):
+
+    def __init__(self, kl_decay_rate, batch_size):
+        self.kl_decay_rate = kl_decay_rate
+        self.batch_size = batch_size
+        pass
+
+    def on_train_batch_end(self, batch, logs=None):
+        # print('For batch {}, kl_weight: {}'.format(batch, self.model.kl_loss_weight))
+        self.model.kl_loss_weight = 1 / (1 + (1 - 0.0001) / 0.0001 * tf.exp(-self.kl_decay_rate * batch * self.batch_size))
+        # tf.summary.scalar('batch_kl_loss_weight', self.model.kl_loss_weight, step=batch * self.batch_size)
+
+
 class Sampling(layers.Layer):
     """Uses (z_mean, z_log_var) to sample z, using the reparametrization trick to allow back propagation."""
 
@@ -19,13 +46,13 @@ class Encoder(layers.Layer):
     def __init__(self,
                  latent_dim=32,
                  intermediate_dim=64,
-                 name='encoder',
+                 name='Encoder',
                  **kwargs):
         super(Encoder, self).__init__(name=name, **kwargs)
         self.dense_proj = layers.Dense(intermediate_dim, activation='relu')
         self.dense_mean = layers.Dense(latent_dim)
         self.dense_log_var = layers.Dense(latent_dim)
-        self.sampling = Sampling()
+        self.sampling = Sampling(name="Gaussian_Sampling")
 
     def call(self, inputs):
         x = self.dense_proj(inputs)
@@ -41,7 +68,7 @@ class Decoder(layers.Layer):
     def __init__(self,
                  original_dim,
                  intermediate_dim=64,
-                 name='decoder',
+                 name='Decoder',
                  **kwargs):
         super(Decoder, self).__init__(name=name, **kwargs)
         self.dense_proj = layers.Dense(intermediate_dim, activation='relu')
@@ -61,18 +88,26 @@ class VariationalAutoEncoder(tf.keras.Model):
                  original_dim=18,
                  intermediate_dim=64,
                  latent_dim=2,
-                 name='autoencoder',
+                 name='Autoencoder',
                  **kwargs):
         super(VariationalAutoEncoder, self).__init__(name=name, **kwargs)
         self.original_dim = original_dim
         self.encoder = Encoder(latent_dim=latent_dim,
                                intermediate_dim=intermediate_dim)
-        self.decoder = Decoder(original_dim, intermediate_dim=intermediate_dim)
+        self.decoder = Decoder(original_dim=original_dim,
+                               intermediate_dim=intermediate_dim)
+        self.kl_loss_weight = 0
+        self.kl_loss = 0
 
     def call(self, inputs):
+        # Forward pass of the Encoder
         z_mean, z_log_var, z = self.encoder(inputs)
+        # Forward pass of the Decoder taken the re-parameterized z latent variable
         reconstructed = self.decoder(z)
-        # Add KL divergence regularization loss.
+
+        # Add KL divergence regularization loss for this forward pass
         kl_loss = - 0.5 * tf.reduce_mean(z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1)
-        self.add_loss(kl_loss)
+
+        self.add_loss(self.kl_loss_weight * kl_loss)
+        self.kl_loss = kl_loss
         return reconstructed
--- vae_training.py
+++ vae_training.py
@@ -1,9 +1,11 @@
 from utils.data_loader_kine_mus import *
-from models.VAE import VariationalAutoEncoder
+from models.VAE import VariationalAutoEncoder, CyclicalAnnealingSchedule, KLLossLogger
 import tensorflow as tf
+from tensorboard.plugins.hparams import api as hp
 
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == '__main__':
 
@@ -16,42 +18,99 @@ if __name__ == '__main__':
     del df
 
     X_train = df_train[[e.value for e in RightHand]].values
+    X_train = StandardScaler(copy=False).fit_transform(X_train)
     X_test = df_test[[e.value for e in RightHand]].values
+    X_test = StandardScaler(copy=False).fit_transform(X_test)
 
-    BATCH_SIZE = 1      # Purely stochastic
+    num_instances = X_train.shape[0]
+    batch_size = 32
+    kl_decay_rate = 0.005
+    lr = 1e-3
+    intermediate_dim = 50
+    latent_dim = 2
+    epochs = 5
+
     train_dataset = tf.data.Dataset.from_tensor_slices(X_train).batch(1)
     test_dataset = tf.data.Dataset.from_tensor_slices(X_test).batch(1)
 
+    vae = VariationalAutoEncoder(original_dim=18, intermediate_dim=intermediate_dim, latent_dim=latent_dim)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 
-    vae = VariationalAutoEncoder(original_dim=18, intermediate_dim=100, latent_dim=2)
+    vae.compile(optimizer,
+                loss=tf.keras.losses.MeanSquaredError(),
+                metrics=[tf.keras.metrics.MeanAbsoluteError(name="reconstruction_MAE")])
+    tf.keras.utils.plot_model(vae, 'vae_model.png', show_shapes=True)
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    # Callbacks
+    log_dir = "models/vae-logs/lr=%.5f-hd=%d-lat_dim=%d-b=%d/" % (lr, intermediate_dim, latent_dim, batch_size)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, update_freq='batch',
+                                                          profile_batch=0)
 
-    mse_loss_fn = tf.keras.losses.MeanSquaredError()
-    loss_metric = tf.keras.metrics.Mean()
+    train_summary_writer = tf.summary.create_file_writer(log_dir + '/train')
 
-    epochs = 10
-
-    # Iterate over epochs.
-    for epoch in range(epochs):
-        print('Start of epoch %d' % (epoch,))
-
-        # Iterate over the batches of the dataset.
-        for step, x_batch_train in enumerate(train_dataset):
-            # print(step)
-            # print(x_batch_train)
-
-            with tf.GradientTape() as tape:
-                reconstructed = vae(x_batch_train)
-                # Compute reconstruction loss
-                loss = mse_loss_fn(x_batch_train, reconstructed)
-                loss += sum(vae.losses)  # Add KLD regularization loss
-
-            # Backpropagate gradients through the network
-            grads = tape.gradient(loss, vae.trainable_weights)
-            optimizer.apply_gradients(zip(grads, vae.trainable_weights))
-
-            loss_metric(loss)
-
-            if step % 100 == 0:
-                print('step %s: mean loss = %s' % (step, loss_metric.result()))
+    vae.fit(X_train, X_train, epochs=epochs, batch_size=batch_size, workers=4,
+            callbacks=[CyclicalAnnealingSchedule(kl_decay_rate=kl_decay_rate, batch_size=batch_size),
+                       tensorboard_callback,
+                       KLLossLogger(train_summary_writer, batch_size=batch_size),
+                       tf.keras.callbacks.TerminateOnNaN()])
+    #
+    # mse_loss_fn = tf.keras.losses.MeanSquaredError()
+    #
+    # loss_metric = tf.keras.metrics.Mean(name='total_loss', dtype=tf.float32)
+    # kl_loss_metric = tf.keras.metrics.Mean(name='KL_loss', dtype=tf.float32)
+    # reconstruction_loss_metric = tf.keras.metrics.Mean(name='reconstruction_loss', dtype=tf.float32)
+    # kl_annealing_metric = tf.keras.metrics.Mean(name='kl_annealing_weight', dtype=tf.float32)
+    #
+    # # Prepare Tensorboard Summary
+    # # current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    # train_log_dir =
+    # # test_log_dir = 'models/vae-logs/' + current_time + '/test'
+    # train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+    # # test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+    #
+    # # Iterate over epochs.
+    # for epoch in range(epochs):
+    #     print('Start of epoch %d' % (epoch,))
+    #
+    #     # Iterate over the batches of the dataset.
+    #     for batch_num, x_batch_train in enumerate(train_dataset):
+    #         # print(step)
+    #         # print(x_batch_train)
+    #
+    #         with tf.GradientTape() as tape:
+    #             reconstructed = vae(x_batch_train)
+    #             # Compute reconstruction loss
+    #             reconstruction_loss = mse_loss_fn(x_batch_train, reconstructed)
+    #             # KL loss is calculated inside the `call` method of the VAE
+    #             kl_loss = sum(vae.losses)
+    #
+    #             # Perform KL cost annealing
+    #             kl_annealing_weight = 1 / (1 + (1 - 0.00001)/0.00001 * tf.exp(-kl_decay_rate * batch_num * batch_size))
+    #
+    #             # Add up the two losses
+    #             loss = reconstruction_loss + kl_annealing_weight * kl_loss
+    #
+    #         # Backpropagate gradients through the network
+    #         grads = tape.gradient(loss, vae.trainable_weights)
+    #         optimizer.apply_gradients(zip(grads, vae.trainable_weights))
+    #
+    #         # Update metrics
+    #         loss_metric(loss)
+    #         kl_loss_metric(kl_loss)
+    #         reconstruction_loss_metric(reconstruction_loss)
+    #         kl_annealing_metric(kl_annealing_weight)
+    #
+    #         # Log to Tensorboard
+    #         with train_summary_writer.as_default():
+    #             tf.summary.scalar('total_loss', loss_metric.result(), step=batch_num * batch_size)
+    #             tf.summary.scalar('KL_loss', kl_loss_metric.result(), step=batch_num * batch_size)
+    #             tf.summary.scalar('reconstruction_loss', reconstruction_loss_metric.result(),
+    #                               step=batch_num * batch_size)
+    #             tf.summary.scalar('kl_annealing_weight', kl_annealing_metric.result(), step=batch_num * batch_size)
+    #
+    #         if batch_num % 100 == 0:
+    #             print('batch_step %s: mean loss = %s' % (batch_num, loss_metric.result()))
+    #
+    #     loss_metric.reset_states()
+    #     kl_loss_metric.reset_states()
+    #     reconstruction_loss_metric.reset_states()
