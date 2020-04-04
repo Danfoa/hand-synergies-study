d709852e7a13facd1187562c201650e4e67f933b
Daniel Ordonez <daniels.ordonez@gmail.com>
2020-04-04 17:52:46 +0200

    cleaned up irrelevant stuff for time series forecasting

--- synergetic_control_test.py
+++ synergetic_control_test.py
@@ -28,7 +28,6 @@ if __name__ == '__main__':
     df.fillna(value=0, inplace=True)
 
     # _________________________________________________________________
-    # Shuffle data for training
     # Split the data into training and testing
     df_train, df_test = train_test_split(df, test_size=0.2, shuffle=False)
     # del df
@@ -42,18 +41,21 @@ if __name__ == '__main__':
     X_test_scaled = test_scaler.fit_transform(X_test)
 
     # _________________________________________________________________
-    # joint_names = [e.value for e in RightHand]
-    # joint_delta_names = [e.value + "_dt" for e in RightHand]
-    # sEMG_names = [e.value for e in sEMG]
+    joint_names = [e.value for e in RightHand]
+    joint_delta_names = [e.value + "_dt" for e in RightHand]
+    sEMG_names = [e.value for e in sEMG]
+    num_emgs = len(sEMG_names)
+
+    # _________________________________________________________________
+
+    # for adl_id in ADLs:
+    #     test_adl = df[df[ExperimentFields.adl_ids.value] == adl_id]
+    #     test_adl = test_adl[test_adl[ExperimentFields.subject.value] > 15]
+    #     shifted = test_adl.shift(periods=-1, axis=0)
     #
-    # # for adl_id in ADLs:
-    # test_adl = df[df[ExperimentFields.adl_ids.value] == adl_id]
-    # test_adl = test_adl[test_adl[ExperimentFields.subject.value] > 15]
-    # shifted = test_adl.shift(periods=-1, axis=0)
-    #
-    # result = df - shifted
-    #
-    # # Create data for sEMG control with `td` time lag
+    #     result = df - shifted
+
+    # Create data for sEMG control with `td` time lag
     # td = 0.01
     # df = test_adl
     # shift_periods = 1
@@ -68,27 +70,6 @@ if __name__ == '__main__':
     # sEMG_activations = df[sEMG_names].values[:-shift_periods]
     # target_positions = joint_values
     #
-    # num_emgs = len(sEMG_names)
-    # latent_dim = 2
-    #
-    # # Load autoencoder trained model
-    # vae = VariationalAutoEncoder(original_dim=18,
-    #                              intermediate_dim=30,
-    #                              latent_dim=2,
-    #                              kl_loss_weight=1.0,
-    #                              trainable=False)
-    #
-    # optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    # vae.compile(optimizer,
-    #             loss=tf.keras.losses.MeanAbsoluteError(),
-    #             metrics=[
-    #                 # tf.keras.metrics.MeanAbsoluteError(name="MAE_loss"),
-    #                 tf.keras.metrics.MeanSquaredError(name="MSE_loss")])
-    #
-    # print("EVAL loaded model:", vae.evaluate(X_test_scaled, X_test_scaled))
-    # vae.load_weights("models/VAE-ld=2/VAE")
-    # print("EVAL loaded model:", vae.evaluate(X_test_scaled, X_test_scaled))
-    #
     # # Build the entire prediction model _____________________________________________
     # emg_inputs = tf.keras.Input(shape=(num_emgs,), name='sEMG_activations')
     # kin_inputs = tf.keras.Input(shape=(len(joint_names),), name='sEMG_activations')
@@ -97,52 +78,44 @@ if __name__ == '__main__':
     #
     # # Prediction part
     # lstm_pred = tf.keras.layers.LSTM(latent_dim*10)(emg_inputs)
-    # delta_mean = tf.keras.layers.Dense(latent_dim)(lstm_pred)
-    # delta_log_var = tf.keras.layers.Dense(latent_dim)(lstm_pred)
-    #
-    # pred_mean = tf.math.add(z_mean, delta_mean)
-    # pred_log_var = tf.math.log(tf.exp(z_log_var + delta_log_var))
-    #
-    # pred_latent_state = vae.encoder.sampling(pred_mean, pred_log_var)
-    #
-    # pred_joint_state = vae.decoder(pred_latent_state)
-    #
+
+
     # prediction_model = tf.keras.Model(inputs=[emg_inputs, kin_inputs], outputs=[pred_joint_state])
-    # #_____________________________________
-    #
-    # tf.keras.utils.plot_model(prediction_model, to_file="prediction_model", show_shapes=True)
+    #_____________________________________
+
+    tf.keras.utils.plot_model(prediction_model, to_file="prediction_model", show_shapes=True)
 
 
 
 
 
 
-    # df_train, df_test = train_test_split(df, test_size=0.2, shuffle=True)
-    #
-    # joint_names = [e.value for e in RightHand]
-    #
-    # # test_adl = df[df[ExperimentFields.adl_ids.value] == 26]
-    # # test_adl = test_adl[test_adl[ExperimentFields.subject.value] == 1]
-    #
-    # emg_labels = [e.value for e in sEMG]
-    # test_adl = pd.melt(test_adl,
-    #                    id_vars=[e.value for e in ExperimentFields],
-    #                    value_vars=emg_labels,
-    #                    var_name="sEMG",
-    #                    value_name="emg_activation")
-    # sn_plot = sns.relplot(x=ExperimentFields.time.value,
-    #                       y="emg_activation",
-    #                       hue=ExperimentFields.phase.value,
-    #                       col="sEMG",
-    #                       units=ExperimentFields.subject.value, estimator=None,
-    #                       col_wrap=4,
-    #                       legend=False,
-    #                       data=test_adl,
-    #                       kind='line',
-    #                       markers=True)
-    # sn_plot.fig.suptitle("ADL %d - ALL Subjects" % adl_id, x=0.87, y=0.25, fontsize=20)
-    # # plt.tight_layout()
-    # plt.legend(bbox_to_anchor=(1.05, 1), loc="lower right", borderaxespad=0.)
-    # plt.savefig("media/MUS/sEMG/ADL%d.png" % adl_id)
-    # plt.show()
-    # # break
+    df_train, df_test = train_test_split(df, test_size=0.2, shuffle=True)
+
+    joint_names = [e.value for e in RightHand]
+
+    # test_adl = df[df[ExperimentFields.adl_ids.value] == 26]
+    # test_adl = test_adl[test_adl[ExperimentFields.subject.value] == 1]
+
+    emg_labels = [e.value for e in sEMG]
+    test_adl = pd.melt(test_adl,
+                       id_vars=[e.value for e in ExperimentFields],
+                       value_vars=emg_labels,
+                       var_name="sEMG",
+                       value_name="emg_activation")
+    sn_plot = sns.relplot(x=ExperimentFields.time.value,
+                          y="emg_activation",
+                          hue=ExperimentFields.phase.value,
+                          col="sEMG",
+                          units=ExperimentFields.subject.value, estimator=None,
+                          col_wrap=4,
+                          legend=False,
+                          data=test_adl,
+                          kind='line',
+                          markers=True)
+    sn_plot.fig.suptitle("ADL %d - ALL Subjects" % adl_id, x=0.87, y=0.25, fontsize=20)
+    # plt.tight_layout()
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="lower right", borderaxespad=0.)
+    plt.savefig("media/MUS/sEMG/ADL%d.png" % adl_id)
+    plt.show()
+    # break
