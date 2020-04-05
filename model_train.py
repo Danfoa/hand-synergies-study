b45c38095ff979046b14ca23eaf4a7d0495577f8
elligalata <lougalata@gmail.com>
2020-04-05 17:42:07 +0300

    Validation set

--- model_train.py
+++ model_train.py
@@ -4,6 +4,7 @@ from regression_model import Regression_Model
 from data_loader_tester import DataLoader
 from utils.kine_mus_tf_dataset import get_windowed_dataset
 import pprint
+from utils.data_loader_kine_mus import sEMG, ADLs, load_subject_data, load_subjects_data
 
 
 if __name__=='__main__':
@@ -16,22 +17,44 @@ if __name__=='__main__':
     n_features = 7
 
     window_sizes = [5, 10, 15, 20]
-    dataset = get_windowed_dataset(dataset_path=DATABASE_PATH, window_size=10, target_joints=target_joints,
-                                   shift_periods=1, subjects=[1], adl_ids=[1], phase_ids=[1, 3])
+    shift_period = 1
+    subjects = [1]
 
+    training_adls = np.random.choice(ADLs, 21, replace=False)
+    test_adls = np.array([e for e in ADLs if not e in training_adls])
 
     for window_size in window_sizes:
+        # Training dataset
+        training_dataset = get_windowed_dataset(dataset_path=DATABASE_PATH,
+                                                window_size=window_size,
+                                                target_joints=target_joints,
+                                                shift_periods=shift_period,
+                                                subjects=subjects,
+                                                adl_ids=training_adls,
+                                                phase_ids=[1, 2, 3])
+        # Validation dataset
+        test_dataset = get_windowed_dataset(dataset_path=DATABASE_PATH,
+                                            window_size=window_size,
+                                            target_joints=target_joints,
+                                            shift_periods=shift_period,
+                                            subjects=subjects,
+                                            adl_ids=test_adls,
+                                            phase_ids=[1, 2, 3])
+
+        training_dataset = training_dataset.map(lambda d: (d["sEMG"], d["angles_delta"]))
+        test_dataset = test_dataset.map(lambda d: (d["sEMG"], d["angles_delta"]))
+
         configurations = Regression_Model(window_size, n_features).configurations
         for model, callback, BATCH_SIZE in configurations:
-            train_dataset = dataset.map(lambda angles_delta, angles, sEMG: (angles, sEMG))
 
-            train_dataset = train_dataset.batch(BATCH_SIZE)
-            # validation_dataset = val_dataset.batch(BATCH_SIZE)
+
+            train_dataset = training_dataset.batch(BATCH_SIZE)
+            validation_dataset = test_dataset.batch(BATCH_SIZE)
             # testing_dataset = test_dataset.batch(BATCH_SIZE)
 
             model.summary()
             model.fit(train_dataset,
-              # validation_data=validation_dataset,
+              validation_data=validation_dataset,
               epochs=epochs,
               callbacks=callback
               )
