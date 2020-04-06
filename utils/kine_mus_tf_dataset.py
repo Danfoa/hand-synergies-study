6448726edd6a20e9677a99c84403510ff805e314
Daniel Ordonez <daniels.ordonez@gmail.com>
2020-04-06 16:20:41 +0200

    Fix import bug

--- utils/kine_mus_tf_dataset.py
+++ utils/kine_mus_tf_dataset.py
@@ -1,51 +1,59 @@
 import tensorflow as tf
+import numpy as np
 import pandas as pd
 from .data_loader_kine_mus import ExperimentFields, DATABASE_PATH, SUBJECTS, ADLs, RightHand, sEMG, load_subject_data
 import pprint
+import matplotlib.pyplot as plt
+
 
 def get_windowed_dataset(dataset_path, target_size, window_size, target_joints, shift_periods=1, subjects=SUBJECTS, adl_ids=ADLs, phase_ids=None):
-def get_windowed_dataset(dataset_path, window_size, target_joints, shift_periods=1, subjects=SUBJECTS, adl_ids=ADLs, phase_ids=None):
     joint_names = [e.value for e in RightHand]
     joint_delta_names = [e.value + "_dt" for e in RightHand]
     sEMG_names = [e.value for e in sEMG]
     experiment_fields = [e.value for e in ExperimentFields]
     num_emgs = len(sEMG_names)
 
+    min_dt, max_dt = 10, -10
     dataset = None
     for subject in subjects:
         for adl in adl_ids:
-            for phase in phase_ids:
-                # print("Loading data drom subject %d, adl %d and phase %d" % (subject, adl, phase))
+            # print("Loading data drom subject %d, adl %d and phase %d" % (subject, adl, phase))
             df = load_subject_data(dataset_path, subject_id=subject, adl_ids=[adl], phase_ids=phase_ids)
-                # Find the angle deltas
-                shifted_df = df.shift(periods=-shift_periods)
-                shifted_df.reset_index(inplace=True, drop=True)
-                df[joint_delta_names] = pd.DataFrame(shifted_df[joint_names].values - df[joint_names].values, index=df.index)
-                df.drop(df.tail(shift_periods).index, inplace=True)
 
             df = df[target_joints + sEMG_names]
-                target_deltas = [d + "_dt" for d in target_joints]
-                df = df[target_joints + target_deltas + sEMG_names]
-
-                # Configure TF dataset
-                tmp_dataset = tf.data.Dataset.from_tensor_slices(df.values)
+            # Configure TF dataset
+            tmp_dataset = tf.data.Dataset.from_tensor_slices(df.values)
             tmp_dataset = tmp_dataset.window(window_size + target_size, shift=1, stride=1, drop_remainder=True)
             tmp_dataset = tmp_dataset.flat_map(lambda x: x.batch(window_size + target_size))
             tmp_dataset = tmp_dataset.map(lambda x: (x[:-target_size, :],
                                                      x[-target_size:, 0]),
-                                                 "sEMG": x[:, 2:]},
-                                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
-                if dataset is None:
-                    dataset = tmp_dataset
-                else:
-                    dataset = dataset.concatenate(tmp_dataset)
+                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            if dataset is None:
+                dataset = tmp_dataset
+            else:
+                dataset = dataset.concatenate(tmp_dataset)
     return dataset
 
 
 def create_time_steps(length):
   return list(range(-length, 0))
 
-    return dataset
+
+def multi_step_plot(history, true_future, prediction):
+  plt.figure(figsize=(12, 6))
+  num_in = np.linspace(0, len(history) / 0.01, len(history))
+  num_out = np.linspace((len(history) + 1) / 0.01, (len(history) + 1 + len(true_future)) / 0.01, len(true_future))
+
+
+  plt.plot(num_in, np.array(history), label='History')
+
+  plt.plot(num_out, np.array(true_future), 'bo',
+           label='True Future')
+  if prediction.any():
+    plt.plot(num_out, np.array(prediction), 'ro',
+             label='Predicted Future')
+  plt.legend(loc='upper left')
+  plt.show()
 
 
 if __name__ == "__main__":
@@ -70,3 +78,4 @@ if __name__ == "__main__":
         print(y.shape)
         x = x.numpy()
         y = y.numpy()
+        multi_step_plot(x[:, 0], y, np.array([0]))
