5a82af118ea78b0cf1705020b20562674aed0135
Daniel Ordonez <daniels.ordonez@gmail.com>
2020-04-14 15:08:07 +0200

    Derivative forecasting again

--- utils/kine_mus_tf_dataset.py
+++ utils/kine_mus_tf_dataset.py
@@ -26,10 +26,20 @@ def get_windowed_dataset(dataset_path, target_size, window_size, target_joints, 
             # print("Loading data drom subject %d, adl %d and phase %d" % (subject, adl, phase))
             df = load_subject_data(dataset_path, subject_id=subject, adl_ids=[adl], phase_ids=phase_ids)
 
-            df = df[target_joints + sEMG_names]
+            # Find the angle deltas
+            shifted_df = df.shift(periods=-shift_periods)
+            shifted_df.reset_index(inplace=True, drop=True)
+            df[joint_delta_names] = pd.DataFrame(shifted_df[joint_names].values - df[joint_names].values,
+                                                 index=df.index)
+            df.drop(df.tail(shift_periods).index, inplace=True)
+
+            #
+            target_deltas = [d + "_dt" for d in target_joints]
+            df = df[target_deltas + sEMG_names]
+            # df = df[target_joints + sEMG_names]
             # Configure TF dataset
             tmp_dataset = tf.data.Dataset.from_tensor_slices(df.values)
-            tmp_dataset = tmp_dataset.window(window_size + target_size, shift=1, stride=1, drop_remainder=True)
+            tmp_dataset = tmp_dataset.window(window_size + target_size, shift=shift_periods, stride=1, drop_remainder=True)
             tmp_dataset = tmp_dataset.flat_map(lambda x: x.batch(window_size + target_size))
             tmp_dataset = tmp_dataset.map(lambda x: (x[:-target_size, :],
                                                      x[-target_size:, 0]),
@@ -38,6 +48,7 @@ def get_windowed_dataset(dataset_path, target_size, window_size, target_joints, 
                 dataset = tmp_dataset
             else:
                 dataset = dataset.concatenate(tmp_dataset)
+    print("")
     return dataset
 
 
@@ -68,16 +79,16 @@ if __name__ == "__main__":
 
     dataset = get_windowed_dataset(dataset_path=DATABASE_PATH, target_size=10, window_size=30,
                                    target_joints=target_joints,
-                                   shift_periods=1, subjects=SUBJECTS, adl_ids=ADLs, phase_ids=None)
+                                   shift_periods=1, subjects=[1], adl_ids=ADLs, phase_ids=None)
 
     df = load_subjects_data(database_path=DATABASE_PATH, subjects_id=SUBJECTS, adl_ids=ADLs,
                             phase_ids=None)
-    count = 0
-    for _ in dataset:
-        count += 1
-
-    print(count)
-    print(df.shape[0])
+    # count = 0
+    # for _ in dataset:
+    #     count += 1
+    #
+    # print(count)
+    # print(df.shape[0])
     # data
     for x, y in dataset.take(1):
         print(x.shape)
